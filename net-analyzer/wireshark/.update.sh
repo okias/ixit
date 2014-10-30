865f01cc9d5ca9de7525ea71dedbd15af7e47a1c
David Heidelberg <david@ixit.cz>
2014-10-30 01:27:05 +0100

    net-analyzer/wireshark: qt5 support added to >= 1.99 in tree
    Signed-off-by: David Heidelberg <david@ixit.cz>

--- net-analyzer/wireshark/.update.sh
+++
@@ -1,3 +0,0 @@
-wget http://wiresharkdownloads.riverbed.com/wireshark/src/
-xml sel -t -v "/_:html/_:body/_:div/_:table/_:tbody/_:tr/_:td/_:a" index.html | grep "^wireshark-" | sed -e "s/^wireshark-//" -e "s/.tar.bz2$//" | sort --version-sort -r | head -n1
-
